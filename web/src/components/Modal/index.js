23489dc01e4fe3b8d234132f8917275027f9f280
brenowss <brenoc.fiorese@hotmail.com.br>
2024-03-15 15:58:42 -0300

    Refactor Loader and Modal components to use ReactPortal

--- web/src/components/Modal/index.js
+++ web/src/components/Modal/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Button from '../Button';
 import { Container, Footer, Overlay } from './styles';
+import ReactPortal from '../ReactPortal';
 
 export default function Modal({
   open,
@@ -18,41 +18,50 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  let container = document.getElementById('modal-root');
 
-        {
-          children && (
-            <div className="modal-body">
-              {children}
-            </div>
-          )
-        }
+  if (!container) {
+    container = document.createElement('div');
+    container.setAttribute('id', 'modal-root');
+    document.body.appendChild(container);
+  }
 
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-          <Button
-            type="button"
-            danger={danger}
-            onClick={onConfirm}
-            isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root')
+          {
+              children && (
+                <div className="modal-body">
+                  {children}
+                </div>
+              )
+            }
+
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
+
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
